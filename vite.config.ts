a32b65ff6ce5d2449c52f56454db2efa9f568613
Marko <marko.janjis@bat.at>
2023-10-09 12:27:40 +0200

    Vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   //we added this for deployment
-  base: '/pivskiCjenik/'
+  base: '/pivskiCjenik/',
+  
 })
