3231f21e8c1f46a0630e3f69683b1873910ee082
Marko <marko.janjis@bat.at>
2023-10-10 10:59:43 +0200

    Hardcoded image

--- src/Components/UI/Avatar.tsx
+++ src/Components/UI/Avatar.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import classes from './Avatar.module.scss';
+import Image from '../../Images/paulaner.png'
 interface IAvatarProps {
     imageSrc: any;
 }
 
 const Avatar = (props: IAvatarProps):JSX.Element => {
     console.log(props.imageSrc)
-    return <img src={props.imageSrc} alt="Avatar" className={classes.avatar}/>
+    return <img src={Image} alt="Avatar" className={classes.avatar}/>
 }
 
 export default Avatar;
